e20376f00fc0ad043cbe40336e2e389b29535992
EduardoBrasil <brasil.eduardo1@gmail.com>
2018-09-12 10:50:56 -0300

    Embedded Endereco at pessoa, and create migration V02

--- src/main/java/com/example/algamoney/api/model/Pessoa.java
+++ src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -1,5 +1,6 @@
 package com.example.algamoney.api.model;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +15,9 @@ public class Pessoa {
 	private long codigo;
 	private String nome;
 	private boolean ativo;
+	
+	@Embedded
+	private Endereço Endereço;
 	
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
